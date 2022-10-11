df3e0d24a04b28e5b84bae37b704f2c67d4766c3
Shukajnhon <87706728+Shukajnhon@users.noreply.github.com>
2022-10-11 12:39:22 +0700

    add route to Work Page, add header and footer in Views folder

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.set("view engine", "ejs");;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
+
+let items = ['Buy Food', 'Cook Food', 'Eat Food'];
+let workListItems = [];
+
 app.get("/", (req, res) => {
     let today = new Date();
 
@@ -19,11 +23,36 @@ app.get("/", (req, res) => {
     }
 
     let day = today.toLocaleDateString('vi-VN', options)
-    console.log(day)
+    // console.log(day)
 
-    res.render("list", { kindOfDay: day })
+    res.render("list", { listTitle: day, newListItems: items })
 
 
+});
+
+app.post("/", (req, res) => {
+    let itemInput = req.body.newItem
+
+    if (req.body.list === 'Work') {
+        workListItems.push(itemInput)
+        res.redirect("/work")
+    } else {
+        items.push(itemInput)
+    }
+
+    res.redirect("/")
+});
+
+
+// route to work page || Work List
+app.get('/work', (req, res) => {
+    res.render('list', { listTitle: 'Work List', newListItems: workListItems })
+});
+
+app.post('/work', (req, res) => {
+    let item = req.body.newItem
+    workListItems.push(item)
+    res.redirect('/work')
 })
 
 
