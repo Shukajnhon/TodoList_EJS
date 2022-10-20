1651681c2b5436d4cffe0eaa1f864f4723fdd970
Shukajnhon <87706728+Shukajnhon@users.noreply.github.com>
2022-10-20 22:38:25 +0700

    Add moongoese, mongodb,insert item into mongodb, delete item when click checked box, custom list name on link direction.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const date = require(__dirname + '/date.js');
 let ejs = require('ejs');
 const app = express();
 app.set("views", "views");
@@ -9,39 +11,114 @@ app.set("view engine", "ejs");;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
 
+// Connect mongodb
+mongoose.connect('mongodb://localhost:27017/todoListDB', { useNewUrlParser: true })
 
-let items = ['Buy Food', 'Cook Food', 'Eat Food'];
-let workListItems = [];
+// Create Schema
+const itemSchema = new mongoose.Schema({
+    name: String
+})
+
+// Create Model
+const Item = mongoose.model("Item", itemSchema)
+
+const item1 = new Item({
+    name: "Welcome to your todoList!"
+})
+
+const item2 = new Item({
+    name: "Hit the + button to add a new item"
+})
+
+const item3 = new Item({
+    name: "<--- Hit the checkbox to delete an item"
+})
+
+const defaultItem = [item1, item2, item3]
+
+const listSchema = {
+    name: String,
+    items: [itemSchema]
+}
+
+const List = mongoose.model("List", listSchema)
+
 
 app.get("/", (req, res) => {
-    let today = new Date();
 
-    let options = {
-        weekday: "long",
-        day: 'numeric',
-        month: 'long',
-    }
+    // Read file, Mongodb
+    Item.find({}, function (err, foundItems) {
+        // Check FoundItems [] or === 0, add Default Item into Database
+        if (foundItems.length === 0) {
+            // Insert Item
+            Item.insertMany(defaultItem, function (err) {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(`Successfully add ${defaultItem}`)
+                }
+                res.redirect("/")
+            })
+        } else {
+            // console.log(foundItems)
+            res.render("list", { listTitle: "Today", newListItems: foundItems })
+        }
 
-    let day = today.toLocaleDateString('vi-VN', options)
-    // console.log(day)
-
-    res.render("list", { listTitle: day, newListItems: items })
-
+    })
 
 });
+// Custom list name on link direction (Local host domain)
+app.get("/:customListName", (req, res) => {
+    const customListName = req.params.customListName
+
+    List.findOne({ name: customListName }, function (err, foundList) {
+        if (!err) {
+            if (!foundList) {
+                // Create a new list
+                const list = new List({
+                    name: customListName,
+                    items: defaultItem
+                })
+                list.save()
+                res.redirect("/" + customListName)
+            } else {
+                // Show an existing list
+                res.render('list', { listTitle: foundList.name, newListItems: foundList.items })
+            }
+        }
+    })
+
+
+
+})
 
 app.post("/", (req, res) => {
-    let itemInput = req.body.newItem
+    const itemName = req.body.newItem
 
-    if (req.body.list === 'Work') {
-        workListItems.push(itemInput)
-        res.redirect("/work")
-    } else {
-        items.push(itemInput)
-    }
+    const newItem = new Item({
+        name: itemName
+    })
+    newItem.save()
 
     res.redirect("/")
+
 });
+
+
+
+// Delete item when click checked box
+app.post("/delete", (req, res) => {
+    const checkedItemId = req.body.checkbox
+
+    Item.findByIdAndRemove(checkedItemId, function (err) {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(`Successfully delete id ${checkedItemId}`)
+        }
+        res.redirect("/")
+    })
+})
 
 
 // route to work page || Work List
